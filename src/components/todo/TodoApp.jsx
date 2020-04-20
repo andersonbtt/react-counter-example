873e441a6e56b16c244ceef0635b081fcff9d90e
Anderson Bittencourt <anderson@MacBook-Air-de-Anderson.local>
2020-04-20 06:04:11 -0300

    Added styling to application

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -35,9 +35,31 @@ class HeaderComponent extends Component{
     }
     render(){
         return (
-            <div>
-                Header <hr/>
-            </div>
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div>
+                        <a href="http://www.bittclouds.com" className="navbar-brand">
+                            bittclouds
+                        </a>
+                    </div>
+                    <ul className="navbar-nav navbar-collapse">
+                        <li>
+                            <Link className="nav-link" to="/welcome/anderson">Home</Link>
+                        </li>
+                        <li>
+                            <Link className="nav-link" to="/todos">Todos</Link>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav navbar-collapse justify-content-end">
+                        <li>
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link className="nav-link" to="/login">Logout</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         )
     }
 }
